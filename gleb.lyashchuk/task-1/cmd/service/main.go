896f937894a4e4719a3a66b7a753a8e27e3feb95
Lyashchuk Gleb <gleblv3011@gmail.com>
2026-09-18 01:14:07 +0300

    [TASK-1] Initial commit

---
+++ gleb.lyashchuk/task-1/cmd/service/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+
+	var a, b, c string
+	fmt.Scan(&a)
+	fmt.Scan(&b)
+	fmt.Scan(&c)
+	numA, errA := strconv.Atoi(a)
+	if errA != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
+	numB, errB := strconv.Atoi(b)
+	if errB != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+	if c == "+" {
+		fmt.Println(numA + numB)
+	} else if c == "-" {
+		fmt.Println(numA - numB)
+	} else if c == "*" {
+		fmt.Println(numA * numB)
+	} else if c == "/" {
+		if numB == 0 {
+			fmt.Println("Division by zero")
+		} else {
+			fmt.Println(numA / numB)
+		}
+	} else {
+		fmt.Println("Invalid operation")
+	}
+}
